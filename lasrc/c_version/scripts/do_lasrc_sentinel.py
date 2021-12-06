5baf0cce8b3c3d5acd695b6f0b0388903ec4c9e5
sharkinsspatial <sharkinsgis@gmail.com>
2021-12-06 15:14:03 -0600

    Reinclude use_orig_aero_alg and subprocess.run for do_lasrc_sentinel.

--- lasrc/c_version/scripts/do_lasrc_sentinel.py
+++ lasrc/c_version/scripts/do_lasrc_sentinel.py
@@ -54,7 +54,12 @@ class SurfaceReflectance():
     #      going to be grabbed from the command line, then it's assumed all
     #      the parameters will be pulled from the command line.
     #######################################################################
-    def runSr (self, xml_infile=None):
+    def runSr(
+        self,
+        xml_infile=None,
+        write_toa=False,
+        use_orig_aero_alg=False
+    ):
         # if no parameters were passed then get the info from the
         # command line
         if xml_infile == None:
@@ -67,6 +72,12 @@ class SurfaceReflectance():
             parser.add_option ("-i", "--xml", type="string",
                 dest="xml",
                 help="name of XML file", metavar="FILE")
+            parser.add_option ("--write_toa", dest="write_toa", default=False,
+                action="store_true",
+                help="write the intermediate TOA reflectance products")
+            parser.add_option ("--use_orig_aero_alg", dest="use_orig_aero_alg", default=False,
+                action="store_true",
+                help="use historical aerosol retrieval")
             (options, args) = parser.parse_args()
     
             # XML input file
@@ -136,22 +147,28 @@ class SurfaceReflectance():
 
         # run surface reflectance algorithm, checking the return status.  exit
         # if any errors occur.
-        cmdstr = ('lasrc --xml={} --aux={} --verbose'
-                  .format(xml_infile, aux_file))
-        msg = 'Executing lasrc command: {}'.format(cmdstr)
-        logger.debug (msg)
-        (exit_code, output) = subprocess.getstatusoutput (cmdstr)
-        logger.info (output)
-        if exit_code != 0:
+        write_toa_opt_str = ''
+        use_orig_aero_alg_str = ''
+
+        if write_toa:
+            write_toa_opt_str = '--write_toa '
+        if use_orig_aero_alg:
+            use_orig_aero_alg_str = '--use_orig_aero_alg '
+        cmd = ['lasrc', f'--xml={xml_infile}', f'--aux={aux_file}',
+               f'{write_toa_opt_str}{use_orig_aero_alg_str}--verbose']
+        msg = 'Executing lasrc command: {}'.format(" ".join(cmd))
+        logger.debug(msg)
+        result = subprocess.run(cmd)
+        logger.info(result.stdout)
+        if result.returncode != 0:
             msg = 'Error running lasrc.  Processing will terminate.'
-            logger.error (msg)
-            os.chdir (mydir)
+            logger.error(msg)
+            os.chdir(mydir)
             return ERROR
-        
         # successful completion.  return to the original directory.
-        os.chdir (mydir)
+        os.chdir(mydir)
         msg = 'Completion of surface reflectance.'
-        logger.info (msg)
+        logger.info(msg)
         return SUCCESS
 
 ######end of SurfaceReflectance class######
